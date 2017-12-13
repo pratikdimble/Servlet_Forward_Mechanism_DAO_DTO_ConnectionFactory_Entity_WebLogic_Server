d59fcb0878e3e0243060bbbfffd7cbdd9e1da19c
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2017-12-13 23:30:48 +0530

    Add files via upload

---
+++ com/pratik/DTO/StudentDTO.java
@@ -0,0 +1,28 @@
+package com.pratik.DTO;
+
+public class StudentDTO {
+private String sid;
+private String sname;
+private String sresult;
+
+public String getSid() {
+return sid;
+}
+public void setSid(String sid) {
+this.sid = sid;
+}
+public String getSname() {
+return sname;
+}
+public void setSname(String sname) {
+this.sname = sname;
+}
+public String getSresult() {
+return sresult;
+}
+public void setSresult(String sresult) {
+this.sresult = sresult;
+}
+
+
+}
